d83c166972db86adb50a7116fe7f2583e131c7e7
Mariya Sergeeva <46738637+islamariya@users.noreply.github.com>
2020-07-20 15:35:14 +0500

    текущее обновление

--- kbs/data/kiosk_modes/kiosk_modes.py
+++
@@ -1,5 +0,0 @@
-class KioskModeNames(object):
-    STANDBYMODE = "Stand by"
-    BEFORECOOKING = "Подготовка к рабочему режиму"
-    COOKINGMODE = "Рабочий режим"
-    TESTINGMODE = "Режим тестирования"
